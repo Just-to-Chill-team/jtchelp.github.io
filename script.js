42d5d5b8b73f2c8573183c13bb571610db5cf273
Mithun <110370009+Mithunhubminepro@users.noreply.github.com>
2023-09-30 15:18:21 +0530

    idk
    Co-Authored-By: DreamXBlade <136983630+DreamXBlade@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const body = document.body;
 const toggleButton = document.getElementById('toggleButton');
 
 toggleButton.addEventListener('click', () => {
-    body.classList.toggle('toggleButton');
+    body.classList.toggle('dark-mode');
 });
